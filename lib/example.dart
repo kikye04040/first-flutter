9ae8b173f45a14596ca1e7e8ca274922d3fe49ae
kikye04040 <kikye04040@gmail.com>
2024-12-02 23:39:21 +0900

    비동기 처리 예

--- lib/example.dart
+++ lib/example.dart
@@ -1,18 +1,14 @@
-// Null Safety
-int? couldReturnNullButDesent() => -3;
-
+// 비동기 처리
 void main() {
-  int? couldBeNullButIsnt = 1;
-  List<int?> listThatCouldHoldNulls = [2, null, 4];
-  List<int?> nullsList;
+  checkVersion();
+  print('end process');
+}
 
-  int a = couldBeNullButIsnt;
-  //int b = listThatCouldHoldNulls.first; int b는 ?가 없으므로 오류
-  int b = listThatCouldHoldNulls.first!;
-  // int c = couldReturnNullButDesent().abs(); null 일 수 있으므로 abs()에서 오류
-  int c = couldReturnNullButDesent()!.abs(); // null 이 아님을 직접 표시
+Future checkVersion() async {
+  var version = await lookUpVersion();
+  print(version);
+}
 
-  print('a is $a.');
-  print('b is $b.');
-  print('b is $c.');
+int lookUpVersion() {
+  return 12;
 }
