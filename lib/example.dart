45b1c730f6f6447b0906d81c753e9e2648d96ef2
kikye04040 <kikye04040@gmail.com>
2024-12-04 00:08:02 +0900

    스트림 통신 예

--- lib/example.dart
+++ lib/example.dart
@@ -1,16 +1,24 @@
-// JSON 데이터 인코딩 예
-import 'dart:convert';
+// 스트림 통신 예
+import 'dart:async';
 
-void main() {
-  var scores = [
-    {'score':40},
-    {'score':80},
-    {'score':100, 'overtime':true, 'special_guest':null}
-  ];
+Future<int> sumStream(Stream<int> stream) async {
+  var sum = 0;
+  await for (var value in stream) {
+    print('sumStream:$value');
+    sum += value;
+  }
+  return sum;
+}
 
-  var jsonText = jsonEncode(scores);
-  print(jsonText ==
-      '[{"score":40},{"score":80},'
-      '{"score":100,"overtime":true,'
-      '"special_guest":null}]');
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i<= to; i++) {
+    print('countStream:$i');
+    yield i;
+  }
+}
+
+main() async {
+  var stream = countStream(10);
+  var sum = await sumStream(stream);
+  print(sum);
 }
