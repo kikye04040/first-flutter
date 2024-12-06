8a181692d323f989c6a7cf6b549ee94b92de1ca3
kikye04040 <kikye04040@gmail.com>
2024-12-06 23:21:41 +0900

    자동차 클래스 구현 예

--- lib/example.dart
+++ lib/example.dart
@@ -1,11 +1,28 @@
-// 구구단 프로그램 예
+// 자동차 클래스 구현 예
 void main() {
-  int i;
-  int j;
+  Car bmw = Car(320, 100000, 'BMW');
+  Car benz = Car(250, 70000, 'BENZ');
+  Car ford = Car(200, 80000, 'FORD');
 
-  for (i = 2; i <= 9; i++) {
-    for (j = 1; j <= 9; j++) {
-      print('$i * $j = ${i + j}');
-    }
+  bmw.saleCar();
+  bmw.saleCar();
+  bmw.saleCar();
+  print(bmw.price);
+}
+
+class Car {
+  int maxSpeed = 0;
+  num price = 0;
+  String name = "";
+
+  Car(int maxSpeed, num price, String name) {
+    this.maxSpeed = maxSpeed;
+    this.price = price;
+    this.name = name;
+  }
+
+  num saleCar() {
+    price = price * 0.9;
+    return price;
   }
 }
