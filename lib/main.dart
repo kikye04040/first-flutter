92d4338b3279997e396263b67630aed15076547c
kikye04040 <kikye04040@gmail.com>
2024-12-09 21:54:00 +0900

    feat: StatefulWidget으로 변경
    StatefulWidget으로 변경하면 상태에 변경이 감지될 때마다 업데이트
    print(value)로 콘솔에 상태 변경 감지 후 true 출력

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,36 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     var switchValue = false;
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      darkTheme: ThemeData.light(),
-      home: Scaffold(
-        body: Center(
-          child: Switch(value: switchValue, onChanged: (value) {
-            switchValue = value;
-    }),
-    ),
-    ));
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+          body: Center(
+            child: Switch(
+                value: switchValue,
+                onChanged: (value) {
+                  print(value);
+                  switchValue = value;
+                }),
+          ),
+        ));
   }
 }
